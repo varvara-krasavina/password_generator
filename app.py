6251cb3dcda059cb046990fe672c43129c4d7044
varvara-krasavina <varvara.krasavina@gmail.com>
2020-09-16 17:50:31 +0300

    Flask API: add GET method w passlen

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, jsonify, request
+
+from password_generator import gen_password
+
+app = Flask(__name__)
+
+@app.route('/passgen/api/v1.0/', methods=['GET'])
+def ttt():
+    response = request.json
+
+    passlen = response['passlen']
+    password = gen_password(passlen)
+
+    return jsonify(
+        {'response': password, 'passlen': passlen}
+    ), 200
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
